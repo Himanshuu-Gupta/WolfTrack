70635a0bd4e034a7538ff8436bbcb30f12d7fe54
Sneha Aradhey <aradheysv@gmail.com>
2021-09-26 02:12:35 -0400

    django_project_models

--- WolfTrack/WolfTrackApp/models.py
+++ WolfTrack/WolfTrackApp/models.py
@@ -5,11 +5,17 @@ class ToDoList(models.Model):
     name = models.CharField(max_length=200)
     def __str__(self):
         return self.name
-    
-class Item(models.Model):
-    ToDoList = models.ForeignKey(ToDoList, on_delete=models.CASCADE)
-    text = models.CharField(max_length=300)
-    completed = models.BooleanField()
 
-    def __str__(self) -> str:
-        return self.text
+
+class User(models.Model):
+    name = models.CharField(max_length=200)
+    def __str__(self):
+        return self.name
+    
+# class Item(models.Model):
+#     ToDoList = models.ForeignKey(ToDoList, on_delete=models.CASCADE)
+#     text = models.CharField(max_length=300)
+#     completed = models.BooleanField()
+#
+#     def __str__(self) -> str:
+#         return self.text
