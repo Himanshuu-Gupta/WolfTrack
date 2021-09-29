0c72d7a892f894429ca5b99398972b783d5c5b32
Shreyansh Arvindbhai Prajapati <shreyansh.prajapati55@gmail.com>
2021-09-29 16:41:13 -0400

    Issue #34: create skeleton for application_dao and profile_dao

---
+++ DAO/profile_dao.py
@@ -0,0 +1,16 @@
+from sql_helper import sql_helper
+
+class profile_dao:
+    def create_profile(self):
+        pass
+
+    def get_profile(self):
+        pass
+
+    def update_profile(self):
+        pass
+
+    def delete_profile(self):
+        pass
+
+
