69085e1cb6e21a3023c479666a638814dbe68df5
Suraj DM <surajdm123@gmail.com>
2021-09-30 15:02:17 -0400

    Fixes: Minor changes to test cases

--- UnitTesting/login_page_testing.py
+++ UnitTesting/login_page_testing.py
@@ -1,6 +1,14 @@
+from unittest.main import main
+from flask import app
 from flask.typing import StatusCode
-from main import app
 import unittest
+import sys, os, inspect
+
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0, parentdir)
+
+from main import app
 
 class FlaskTest(unittest.TestCase):
 
@@ -14,15 +22,14 @@ class FlaskTest(unittest.TestCase):
     #check if content returned is application/json
     def test_index_content(self):
         tester = app.test_client(self)
-        response = tester.get("/user")
-        self.assertEqual(response.content_type, "application/json")
+        response = tester.get("/login")
+        self.assertEqual(response.content_type, "text/html")
 
     #check data returned
     def test_index_data(self):
         tester = app.test_client(self)
         response = tester.get("/login")
-        print(response.data)
         self.assertEqual(b'WolfTrack' in response.data, True)
 
- if __name__=="__main__":
+if __name__=="__main__":
      unittest.main()
--- UnitTesting/user_details_testing.py
+++ UnitTesting/user_details_testing.py
@@ -1,7 +1,15 @@
 from flask.typing import StatusCode
-from main import app
-import unittest
 
+import unittest
+import sys, os, inspect
+
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0, parentdir)
+
+from main import app
+
+# Testing template
 class FlaskTest(unittest.TestCase):
 
     #check if response is 200
