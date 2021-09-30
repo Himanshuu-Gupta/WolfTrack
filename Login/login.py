f6b66b0541b121a004de94843d08eda021cb271b
Shreyansh Arvindbhai Prajapati <shreyansh.prajapati55@gmail.com>
2021-09-29 21:14:15 -0400

    Fix session details by changing user_id to email_id

--- Login/login.py
+++ Login/login.py
@@ -5,14 +5,42 @@ from datetime import datetime, timedelta
 login_route = Blueprint('admin', __name__)
 login_manager = LoginManager()
 headers = {'Content-Type': 'text/html'}
+from DAO.sql_helper import sql_helper
+db = sql_helper()
+
+data = {
+    "wishlist": ["Microsoft", "Google", "Uber"],
+    "inprogress": ["Twitter", "Pearson"],
+    "applied": ["Amazon", "NetApp"],
+    "offers": ["Perfios"]
+}
+
+upcoming_events = [
+    {"duedate": "28th Sept, 2021",
+     "company": "Apple"
+     },
+    {"duedate": "19th Dec, 2021",
+     "company": "Microsoft"
+     },
+    {"duedate": "21st Dec, 2021",
+     "company": "Amazon"
+     },
+    {"duedate": "21st Dec, 2021",
+     "company": "Amazon"
+     },
+    {"duedate": "21st Dec, 2021",
+     "company": "Amazon"
+     }
+]
+
 
 @login_route.record_once
 def on_load(state):
     login_manager.init_app(state.app)
 
 @login_manager.user_loader
-def load_user(userid):
-    return session['userinfo'][userid]
+def load_user(id):
+    return User(session['userinfo']['user_name'])
 
 @login_route.before_app_request
 def before_request():
@@ -23,104 +51,40 @@ def before_request():
             pass
 
 class User(UserMixin):
-    pass
+    def __init__(self, username) -> None:
+        self.username = username
+
+    def is_active(self):
+        return self.active
+
+    def is_anonymous(self):
+        return False
+
+    def is_authenticated(self):
+        return True
 
 
 def is_valid(username, password):
     ''' Validate the username and password with DB '''
-    pass
-
+    query = None
+    query = 'SELECT id FROM user_login as u INNER JOIN user ON u.user_id=user.user_id WHERE user.email ' \
+            '='+username+'and user.password='+password
+    result = db.run_query(query)
+    if not result:
+        return False
+    session['userinfo'] = {'email_id': username, 'user_id': result}
+    return True
 
 @login_route.route('', methods=["GET", "POST"])
 def login():
     if request.method == 'GET':
         return make_response(render_template('login.html'), 200, headers)
-    username = request.json['Username']
-    password = request.json['Password']
-    if is_valid(username, password):
-        pass
-    user = User()
-    session['userinfo'] = {'userid': username}
+    username = request.form['username']
+    password = request.form['password']
+    if not is_valid(username, password):
+        raise 401
+    user = User(username)
     user.id = username
     if request.method == 'POST':
         login_user(user)
-    return render_template('home.html')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# from flask import Blueprint, make_response, session, request, jsonify, current_app, redirect, url_for, render_template
-# from datetime import timedelta, datetime
-# from flask_login import login_user, LoginManager
-# from Login.User import User, user_blueprint
-# import jwt
-# from templates import *
-#
-# auth_token = Blueprint('auth_token', __name__)
-# auth_token.register_blueprint(user_blueprint)
-#
-# @auth_token.route('/', methods=['GET', 'POST'])
-# def home():
-#     return make_response('Go to login page /login')
-#
-# @auth_token.before_app_request
-# def before_request():
-#     session.modified = True
-#
-#     # current_app.permanent_session_lifetime = timedelta(minutes=30)
-#     # if request.endpoint != 'auth_token.login':
-#     #     if 'user' not in session:
-#     #         if 'db_con' in session:
-#     #             session.pop('db_con').close()
-#
-# def isValid():
-#     pass
-#
-#
-#
-# """Verifying user login against LDAP and token generation"""
-# @auth_token.route('/login', methods=['GET','POST'])
-# def login():
-#     if request.method == 'GET':
-#         print('OK')
-#         return render_template('home.html')
-#     username = request.json.get('Username')
-#     password = request.json.get('Password')
-#     # username = request.form['username']
-#     # password = request.form['password']
-#     if not isValid():
-#         pass
-#
-#     session['user'] = username
-#     token = jwt.encode({'_id': username, 'exp': datetime.utcnow() + timedelta(hours=720)},
-#                        current_app.config['SECRET_KEY'], algorithm='HS256')
-#
-#     user = User()
-#     user.id = token
-#     session['token'] = token
-#     login_user(username)
-#
-#     return redirect(url_for('auth_token.homepage'))
-#
-#
-#
-#
-#
-#
-#
-# @auth_token.route('/home', methods=['GET'])
-# def homepage():
-#     return render_template('home.html')
-#
+    return make_response(render_template('home.html', data=data, upcoming_events=upcoming_events), 301, headers)
