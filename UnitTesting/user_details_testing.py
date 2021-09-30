486b10a7a9eec15d053f912110ccf580b474d6cf
Himanshu-Gupta <himanshu199586@gmail.com>
2021-09-30 02:57:40 -0400

    making blueprint for testing

--- UnitTesting/user_details_testing.py
+++ UnitTesting/user_details_testing.py
@@ -6,23 +6,14 @@ class FlaskTest(unittest.TestCase):
 
     #check if response is 200
     def test_index(self):
-        tester = app.test_client(self)
-        response = tester.get("/login")
-        statuscode = response.status_code
-        self.assertEqual(statuscode, 200)
-
+        pass
+    
     #check if content returned is application/json
     def test_index_content(self):
-        tester = app.test_client(self)
-        response = tester.get("/user")
-        self.assertEqual(response.content_type, "application/json")
+        pass
 
     #check data returned
     def test_index_data(self):
-        tester = app.test_client(self)
-        response = tester.get("/login")
-        print(response.data)
-        self.assertEqual(b'WolfTrack' in response.data, True)
-
- if __name__=="__main__":
-     unittest.main()
+        pass
+if __name__=="__main__":
+    unittest.main()
